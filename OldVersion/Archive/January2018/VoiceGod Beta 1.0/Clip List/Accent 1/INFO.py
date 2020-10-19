9af92133aa8d365be8cea6cedfd3011bbcf9873d
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-19 11:11:25 -0700

    INFO.py
    Info about the setup.

---
+++ OldVersion/Archive/January2018/VoiceGod Beta 1.0/Clip List/Accent 1/INFO.py
@@ -0,0 +1,4 @@
+# Info
+# Due to GitHubs truncation at 1000 items, I had to split this projects directory into 17 parts.
+# In the original, 1-100 and all other labeled directories in this format didn't exist, all 1700 folders were in the root of the directory.
+# The `IGNORE.md` files didn't exist either, they were added due to not being able to upload blank directories to GitHub.
